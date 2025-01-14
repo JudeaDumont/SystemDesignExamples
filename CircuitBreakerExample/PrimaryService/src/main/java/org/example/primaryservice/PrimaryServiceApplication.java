c78064f00e7c90104518704c695ccfd9896279e5
dumont, judea <dumontjudea2@gmail.com>
2025-01-13 16:18:33 -0800

    Initial commit

---
+++ CircuitBreakerExample/PrimaryService/src/main/java/org/example/primaryservice/PrimaryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.primaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimaryServiceApplication.class, args);
+    }
+
+}
